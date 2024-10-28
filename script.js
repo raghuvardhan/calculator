ffd4ab88ede76335fb1ea586a7424e1cffe385f6
Raghuvardhan Karanam <karanamraghuvardhan12@gmail.com>
2024-10-28 12:15:25 +0530

    intial calculator

---
+++ script.js
@@ -0,0 +1,36 @@
+let currentInput = '';
+
+function appendNumber(number) {
+    console.log("append");
+    currentInput += number;
+    updateDisplay();
+}
+
+function appendOperator(operator) {
+    if (currentInput === '') return;
+    const lastChar = currentInput[currentInput.length - 1];
+    if ('+-*/%'.includes(lastChar)) {
+        currentInput = currentInput.slice(0, -1);
+    }
+    currentInput += operator;
+    updateDisplay();
+}
+
+function clearDisplay() {
+    currentInput = '';
+    updateDisplay();
+}
+
+function calculate() {
+    try {
+        const result = eval(currentInput.replace(/%/g, '/100'));
+        currentInput = result.toString();
+    } catch (error) {
+        currentInput = 'Error';
+    }
+    updateDisplay();
+}
+
+function updateDisplay() {
+    document.getElementsByClassName('screen')[0].innerText = currentInput || '0';
+}
